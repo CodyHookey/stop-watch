2ea7e6faf9d9684ba95fc87011755ee32b0f7b6e
Cody Hookey <codyhookey@outlook.com>
2025-03-08 15:22:03 +0000

    added functionality to the stop watch

--- src/script.js
+++ src/script.js
@@ -4,6 +4,40 @@ class StopWatch extends React.Component {
     this.state = {
       timePassedInMs: 0,
     };
+
+    this.timer = null;
+
+    this.start = this.start.bind(this);
+    this.stop = this.stop.bind(this);
+    this.reset = this.reset.bind(this);
+  }
+
+  start() {
+    if (!this.timer) {
+      let startTime = Date.now();
+      this.timer = setInterval(() => {
+        const stopTime = Date.now();
+        const timePassedInMs = stopTime - startTime + this.state.timePassedInMs;
+
+        this.setState({
+          timePassedInMs,
+        });
+
+        startTime = stopTime;
+      }, 250); // Executed every 250 millisecond
+    }
+  }
+
+  stop() {
+    window.clearInterval(this.timer);
+    this.timer = null;
+  }
+
+  reset() {
+    this.stop();
+    this.setState({
+      timePassedInMs: 0,
+    });
   }
 
   render() {
@@ -16,9 +50,15 @@ class StopWatch extends React.Component {
           {Math.floor(this.state.timePassedInMs / 1000)}s
         </h2>
         <div className="d-flex justify-content-center">
-          <button className="btn btn-outline-primary mr-2">Start</button>
-          <button className="btn btn-outline-danger mr-2">Stop</button>
-          <button className="btn btn-outline-warning">Reset</button>
+          <button className="btn btn-outline-primary mr-2" onClick={this.start}>
+            Start
+          </button>
+          <button className="btn btn-outline-danger mr-2" onClick={this.stop}>
+            Stop
+          </button>
+          <button className="btn btn-outline-warning" onClick={this.reset}>
+            Reset
+          </button>
         </div>
       </div>
     );
